e8998928c6d8a9d240d0becf168c88419b746625
muhammadrayhanwidayat <murayat99@gmail.com>
2024-03-19 15:12:51 +0700

    menambah looping for

--- LoopingFor/LoopingFor.cpp
+++ LoopingFor/LoopingFor.cpp
@@ -5,4 +5,18 @@ int main()
 {
     int i;
     int arr[5];
+    for (i = 60; i > 10; i -= 10) {
+        cout << i << "selamat pagi dunia" << endl;
+    }
+
+    cout << "Nilai i terakhir" << i << endl;
+
+    for (i = 0; i < 5; i++) {
+        cout << "masukan nilai index ke-" << i << ":";
+        cin >> arr[i];
+    }
+    for (i = 0; i < 5; i++) {
+        cout << "Data array ke-" << i << ":" << arr[i] << endl;
+    }
+    return 0;
 }
